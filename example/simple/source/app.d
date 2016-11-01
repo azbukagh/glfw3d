9fa057664efbc46ef10d76ed3754a185594f208d
Azbuka <azbukapublic@yahoo.com>
2016-11-01 12:44:18 +0100

    glfw3dLogger renamed, glfw3dErrorHappened added

--- example/simple/source/app.d
+++ example/simple/source/app.d
@@ -129,4 +129,3 @@ void main() {
 
 	glfw3dTerminate();
 }
-
--- source/glfw3d/Main.d
+++ source/glfw3d/Main.d
@@ -5,17 +5,26 @@ module glfw3d.Main;
 
 import std.experimental.logger;
 import glfw3d.glfw3;
+import std.string : fromStringz;
 
-__gshared Logger glfw3dLog;
+shared static this() {
+	glfw3dLogger = sharedLog;
+}
+
+/**
+*	Logger for glfw3d
+*/
+__gshared Logger glfw3dLogger;
+
+/**
+*	`true` if any error happened
+*/
+__gshared bool glfw3dErrorHappened = false;
 
 /**
 *	Terminates glfw3d
 */
 alias glfw3dTerminate = glfwTerminate;
-
-shared static this() {
-	glfw3dLog = sharedLog;
-}
 
 /**
 *	glfw3d throws this exception on errors
@@ -43,15 +52,14 @@ int[3] glfw3dVersion() {
 *	Returns: GLFW version string
 */
 string glfw3dVersionString() {
-	import std.string : fromStringz;
 	return cast(string) glfwGetVersionString().fromStringz;
 }
 
 private template Error(string e) {
 	import std.string : fromStringz;
-	const char[] Error = 
-	"case GLFW_" ~ e ~ ":"
-	~" try {glfw3dLog.log(\"GLFW error: "~ e ~"\");} catch {} break;";
+	const char[] Error =
+	"case GLFW_" ~ e ~ ":" ~
+	"glfw3dErrorHappened = true; try {glfw3dLogger.log(\"GLFW error: \", desc.fromStringz);} catch {} break;";
 }
 
 private extern(C) nothrow void glfw3dErrorCallback(int error, const(char)* desc) {
@@ -59,16 +67,16 @@ private extern(C) nothrow void glfw3dErrorCallback(int error, const(char)* desc)
 		mixin(Error!("VERSION_UNAVAILABLE"));
 		mixin(Error!("NO_CURRENT_CONTEXT"));
 		mixin(Error!("FORMAT_UNAVAILABLE"));
-//		mixin(Error!("NO_WINDOW_CONTEXT"));
+		mixin(Error!("NO_WINDOW_CONTEXT"));
 		mixin(Error!("NOT_INITIALIZED"));
 		mixin(Error!("API_UNAVAILABLE"));
 		mixin(Error!("PLATFORM_ERROR"));
 		mixin(Error!("INVALID_VALUE"));
 		mixin(Error!("OUT_OF_MEMORY"));
 		mixin(Error!("INVALID_ENUM"));
-		
+
 		default:
-			try {glfw3dLog.log("GLFW error: unknown");} catch {}
+			try {glfw3dLogger.log("GLFW error: unknown");} catch {}
 			break;
 	}
 }
@@ -80,7 +88,7 @@ private extern(C) nothrow void glfw3dErrorCallback(int error, const(char)* desc)
 *		l = logger for glfw3d
 */
 void glfw3dInit(Logger l) {
-	glfw3dLog = l;
+	glfw3dLogger = l;
 	glfw3dInit();
 }
 /// ditto
@@ -88,9 +96,6 @@ void glfw3dInit() {
 	if(!glfwInit()) {
 		glfw3dTerminate();
 		throw new glfw3dException("GLFW initialization failed");
-	} else {
-		glfwSetErrorCallback(&glfw3dErrorCallback);
 	}
-	debug glfw3dLog.log("glfw3d initialized");
+	glfwSetErrorCallback(&glfw3dErrorCallback);
 }
-
